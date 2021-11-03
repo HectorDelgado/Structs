c3bc1baead78d638426ee42dd9c169cdd509f8cc
Hector Delgado <Hector.d.delgado@gmail.com>
2021-11-02 23:58:19 -0700

    Added basic files

---
+++ Structs/Structs.h
@@ -0,0 +1,18 @@
+//
+//  Structs.h
+//  Structs
+//
+//  Created by Hector Delgado on 11/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Structs.
+FOUNDATION_EXPORT double StructsVersionNumber;
+
+//! Project version string for Structs.
+FOUNDATION_EXPORT const unsigned char StructsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Structs/PublicHeader.h>
+
+
