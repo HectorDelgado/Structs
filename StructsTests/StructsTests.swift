dd116004c430fcb3aa56f61758742e2a6dcecd70
Hector Delgado <Hector.d.delgado@gmail.com>
2021-11-03 00:30:47 -0700

    Added empty test

--- StructsTests/StructsTests.swift
+++ StructsTests/StructsTests.swift
@@ -17,17 +17,28 @@ class StructsTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testAppendInLinkedListCount() throws {
+        // Given
+        var singlyLinkedList = SinglyLinkedList<Int>()
+        let range = 1...10
+        
+        // When
+        for i in range {
+            singlyLinkedList.append(i)
         }
+        
+        // Then
+        XCTAssert(singlyLinkedList.count == range.count)
     }
+    
+    func
+
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
